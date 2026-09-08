47e7df4955ed120e55b6cca2b3eb54cee7adc849
ElaraStellaria <Elara.stellaria@gmail.com>
2026-09-09 01:29:30 +0200

    Update project

--- scenes/characters/player/chopping_state.gd
+++ scenes/characters/player/chopping_state.gd
@@ -2,6 +2,11 @@ extends NodeState
 
 @export var player: Player
 @export var animated_sprite_2d: AnimatedSprite2D
+@export var hit_component_collison_shape : CollisionShape2D
+
+func _ready() -> void:
+	hit_component_collison_shape.disabled = true
+	hit_component_collison_shape.position = Vector2(0 , 0);
 
 func _on_process(_delta : float) -> void:
 	pass
@@ -19,15 +24,22 @@ func _on_next_transitions() -> void:
 func _on_enter() -> void:
 	if player.player_direction == Vector2.UP:
 		animated_sprite_2d.play("chopping_back")
+		hit_component_collison_shape.position = Vector2(0 , -18)
 	elif player.player_direction == Vector2.RIGHT:
 		animated_sprite_2d.play("chopping_right")
+		hit_component_collison_shape.position = Vector2(9 , -1)
 	elif player.player_direction == Vector2.DOWN:
 		animated_sprite_2d.play("chopping_front")
+		hit_component_collison_shape.position = Vector2(0 , 3)
 	elif player.player_direction == Vector2.LEFT:
 		animated_sprite_2d.play("chopping_left")
+		hit_component_collison_shape.position = Vector2(-9 , -1)
 	else:
 		animated_sprite_2d.play("chopping_front")
+	
+	hit_component_collison_shape.disabled = false
 
 
 func _on_exit() -> void:
-	pass
+	animated_sprite_2d.stop()
+	hit_component_collison_shape.disabled = true
---
+++ scenes/components/damage_component.gd
@@ -0,0 +1,13 @@
+class_name DamageComponent
+extends Node2D
+
+@export var mix_damage = 1
+@export var current_damage = 0
+
+signal mix_damage_reached
+
+func apple_damage(damage: int) -> void:
+	current_damage = clamp(current_damage + damage, 0, mix_damage)
+	
+	if current_damage == mix_damage:
+		mix_damage_reached.emit()
